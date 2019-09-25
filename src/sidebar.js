113ecc0f7fe2573b354011b7be08323225e54a63
leirelitw <litwin.leire@gmail.com>
2019-09-25 22:45:05 +0200

    adding sidebar ids
    adding id's to the sidebar elements in the sidebar.js

--- src/sidebar.js
+++ src/sidebar.js
@@ -11,15 +11,20 @@ const SidebarDrawing = () => (
             visible
             width='thin'
         >
-            <Menu.Item as='a'>
+
+            <Menu.Item as='a' id={"clear_canvas_button"}>
+                <Icon name='refresh' />
+                Clear canvas
+            </Menu.Item>
+            <Menu.Item as='a' id={"pencil_type_button"}>
                 <Icon name='paint brush' />
                 Pencil
             </Menu.Item>
-            <Menu.Item as='a'>
+            <Menu.Item as='a' id={"pencil_size_button"}>
                 <Icon name='circle' />
                 Size
             </Menu.Item>
-            <Menu.Item as='a'>
+            <Menu.Item as='a' id={"pencil_color_button"}>
                 <Icon name='tint' />
                 Color
             </Menu.Item>
